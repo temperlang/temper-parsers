9339ca01f5a7c54587ed96c05a9142ecde6cb6e7
Shaw Summa <shawsumma@gmail.com>
2024-02-18 05:49:44 -0500

    format
    Signed-off-by: Shaw Summa <shawsumma@gmail.com>

--- index.js
+++ index.js
@@ -1,31 +1,61 @@
 
 import {Parser} from 'temper-earley/parser';
+import {format} from 'temper-earley/tree';
 
 const parser = new Parser(`
 
-start: topLevel*
+start: (topLevel ";"!)* expr?
 
-blockBody: (topLevel ";") expr?
+blockBody: (topLevel ";"!)* expr?
 
-topLevel: (decorator? varDecl) ";"!
+topLevel?: varDecl | expr | returnStmt
 
-varDecl: ("export" | "public" | "private" | "static" | "get" | "set" | "var" | "let" | "const")* ident ("="! expr)?
+returnStmt: "return"! expr?
 
-expr: logic
+varDecl: ("export" | "public" | "private" | "static" | "get" | "set" | "var" | "let" | "const")+ ident ("="! expr)?
 
-logic: logic ("&&" | "||") cmp | cmp
+expr?: logic
 
-cmp: add ("<" | ">" | "<=" | ">=" | "==" | "!=") add | add
+logic : logic ("&&" | "||") cmp -> binary
+logic?: cmp
 
-add: add [+-] mul | mul
+cmp: add ("<" | ">" | "<=" | ">=" | "==" | "!=") add -> binary
+cmp?: add
 
-mul: mul [*/%] pow | pow
+add: add [+-] mul -> binary
+add?: mul
 
-pow: single "**" pow | single
+mul: mul [*/%] pow -> binary
+mul?: pow
 
-single: ident | number | "("! expr ")"!
+pow: prefix "**" pow -> binary
+pow?: prefix
 
-number: int | float
+prefix: [-+] postfix -> unary
+prefix?: postfix
+
+postfix: postfix "["! expr "]"! -> index
+postfix: postfix "."! ident -> dot
+postfix: postfix args -> call
+postfix: "new"! type args -> new
+postfix?: single
+
+args?: "("! (expr ","!)* expr? ")"! blockArg*
+args?: blockArg+
+blockArg: "{"! blockSig ";;"! blockBody "}"!
+
+single?: ident | int | float | array | "("! expr ")"!
+
+array: "["!  "]"!
+
+blockSig: "("! (maybeTypedParam ","!)* maybeTypedParam? ")"! ":"! type
+blockSig: "("! (maybeTypedParam ","!)* maybeTypedParam? ")"! -> blockSigAutoReturn
+
+maybeTypedParam: ident ":"! type -> typedParam
+maybeTypedParam: ident -> untypedParam
+
+type: ident
+type: ident "<"! (type ","!)* type? ">"! -> genericType 
 
 int: INT
 
@@ -33,18 +63,23 @@ float: FLOAT
 
 ident: CNAME
 
-FLOAT: INTEGER "." INTEGER
-INT: "0"
-    | "-" [123456789] DIGIT*
-    | [123456789] DIGIT*
+FLOAT: INT "." INT
+INT: "0" | [123456789] DIGIT*
 
 ignore: WHITESPACE
 
 `);
 
-console.log(parser.parse(`
+const data = parser.parse(`
 
-    let x = 3;
-    let y = x;
+    var builder = new ListBuilder<Int>();
+    builder.add(1);
+    builder.add(2);
+    var twice = builder.map { (x);;
+        return x * 2;
+    };
+    console.log(twice[0]);
 
-`).toString());
+`);
+
+console.log(format(data));
