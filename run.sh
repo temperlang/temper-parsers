261e2715bec2088202bab900f8ee7922e778c403
Shaw Summa <shawsumma@gmail.com>
2024-02-19 04:52:51 -0500

    immediate trees
    Signed-off-by: Shaw Summa <shawsumma@gmail.com>

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
 time='time -f"%es %Mk %P"'
-args='grammars/temper input/*.temper'
+args='grammars/temper src/*.temper'
 
-which node > /dev/null  && echo "--- node ---" && sh -c "$time node entry/main.js $args"
-which bun > /dev/null  && echo "--- bun ---" && sh -c "$time bun entry/main.js $args"
-which pypy3 > /dev/null  && echo "--- pypy3 ---" && sh -c "$time pypy3 entry/main.py $args"
-which python3 > /dev/null  && echo "--- python3 ---" && sh -c "$time python3 entry/main.py $args"
-# which luajit > /dev/null  && echo "--- luajit ---" && sh -c "$time luajit entry/main.lua $args"
-# which lua > /dev/null  && echo "--- lua ---" && sh -c "$time lua entry/main.lua $args"
+which node > /dev/null  && echo "--- node ---" && sh -c "$time node entry/main.js $args > out.node.txt"
+which bun > /dev/null  && echo "--- bun ---" && sh -c "$time bun entry/main.js $args > out.bun.txt"
+which pypy3 > /dev/null  && echo "--- pypy3 ---" && sh -c "$time pypy3 entry/main.py $args > out.pypy3.txt"
+which python3 > /dev/null  && echo "--- python3 ---" && sh -c "$time python3 entry/main.py $args > out.python3.txt"
+which luajit > /dev/null  && echo "--- luajit ---" && sh -c "$time luajit entry/main.lua $args > out.luajit.txt"
+which lua > /dev/null  && echo "--- lua ---" && sh -c "$time lua entry/main.lua $args > out.lua.txt"
