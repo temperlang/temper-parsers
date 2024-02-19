a758504dbb3bdaea2c5753cc44f62c6c77c20aae
Shaw Summa <shawsumma@gmail.com>
2024-02-19 14:14:29 -0500

    save4
    Signed-off-by: Shaw Summa <shawsumma@gmail.com>

--- entry/lex.js
+++ entry/lex.js
@@ -7,9 +7,12 @@ import {argv} from 'process';
 const parser = new Parser(String(readFileSync(argv[2])));
 
 for (let i = 3; i < argv.length; i++) {
-    const data = parser.lex(String(readFileSync(argv[i])));
-    
-    for (const tok of data) {
-        console.log(tok.toString());
+    const src  = String(readFileSync(argv[i]));
+    const data = parser.lex(src);
+    const dest = data.map(x => x.value).join("");
+    if (src !== dest) {
+        console.log(`fail: ${argv[i]}`)
+    } else {
+        console.log(`succeed: ${src.length / data.length}`);
     }
 }
