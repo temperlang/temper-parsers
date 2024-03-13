3efef6c13ec2075aa7df616e11e8da54632e4362
Shaw Summa <shawsumma@gmail.com>
2024-03-13 09:25:14 -0400

    floyd normal form start
    Signed-off-by: Shaw Summa <shawsumma@gmail.com>

--- entry/main.js
+++ entry/main.js
@@ -1,24 +1,83 @@
 #!/usr/bin/env node
 
 import {readFileSync} from 'fs';
-import {Parser, format, Tree} from 'temper-parsers/all-exports.js';
+import {Parser, Tree, format} from 'temper-parsers/all-exports.js';
 import {argv} from 'process';
 
-const parser = new Parser(String(readFileSync(argv[2])));
+let n = 1;
+let mode = 'rules-rec';
+let echo = false;
+let time = false;
 
-for (let i = 3; i < argv.length; i++) {
-    let data = null;
-    try {
-        data = parser.parse(String(readFileSync(argv[i])));
-    } catch (e) {
-        console.error(e);
-        console.log(`fail ${argv[i]}`);
+while (argv[2].startsWith('-')) {
+    if (argv[2] == '-n') {
+        n = Number(argv[3]);
+        argv.splice(2, 2);
+    } else if (argv[2] == '-e') {
+        echo = true;
+        argv.splice(2, 1);
+    } else if (argv[2] == '-t') {
+        time = true;
+        argv.splice(2, 1);
+    } else if (argv[2] == '-m') {
+        mode = argv[3];
+        argv.splice(2, 2);
+    } else {
+        break;
     }
-    if (data != null) {
-        if (data instanceof Tree) {
-            console.log(format(data));
-        } else {
-            console.log(`fail ${argv[i]}`);
+}
+
+let parser;
+{
+    const src = String(readFileSync(argv[2]));
+    let start = new Date();
+    parser = new Parser(src);
+    let end = new Date();
+    if (time) {
+        console.log(`grammar: ${end - start}ms`);
+    }
+}
+
+parser.mode = mode;
+
+{
+    let lines = 0;
+    let chars = 0;
+
+    let strs = [];
+    for (let i = 3; i < argv.length; i++) {
+        let file = argv[i];
+        let data = String(readFileSync(argv[i]));
+        strs.push([file, data]);
+        lines += data.split('\n').length;
+        chars += data.length;
+    }
+
+    let start = new Date();
+    for (let i = 0; i < n; i++) {
+        for (const [file, data] of strs) {
+            let res = null;
+            try {
+                res = parser.parse(data);
+            } catch (e) {
+                console.error(e);
+                console.log(`fail ${file}`);
+            }
+            if (res != null) {
+                if (res instanceof Tree) {
+                    if (echo) {
+                        console.log(format(res));
+                    }
+                } else {
+                    console.log(`fail ${file}`);
+                }
+            }
         }
+    }
+    let end = new Date();
+    if (time) {
+        console.log(`files: ${end - start}ms`);
+        console.log(`${lines * n / (end - start) * 1000 |0} ln/s`);
+        console.log(`${chars * n / (end - start) |0} chars/ms`);
     }
 }
--- entry/main.lua
+++ entry/main.lua
@@ -12,8 +12,85 @@ local function read(name)
     return ret
 end
 
-local p = parser.Parser(read(arg[1]))
-for i=2, #arg do
-    local data = p:parse(read(arg[i]))
-    print(tree.format(data))
+local function line_count(data)
+    local n = 0
+    for i=1, string.len(data) do
+        if string.byte(data, i, i) == 10 then
+            n = n + 1
+        end
+    end
+    return n
+end
+
+local n = 1
+local mode = 'rules-rec'
+local echo = false
+local time = false
+
+local head = 1
+while arg[head] ~= nil and string.sub(arg[head], 1, 1) == '-' do
+    if arg[head] == '-n' then
+        n = tonumber(arg[head + 1])
+        head = head + 2
+    elseif arg[head] == '-e' then
+        echo = true
+        head = head + 1
+    elseif arg[head] == '-t' then
+        time = true
+        head = head + 1
+    elseif arg[head] == '-m' then
+        mode = arg[head + 1]
+        head = head + 2
+    else
+        break
+    end
+end
+
+local t1 = os.clock()
+local p = parser.Parser(read(arg[head]))
+local t2 = os.clock()
+if time then
+    print(string.format('grammar: %sms', math.floor((t2-t1)*1000)))
+end
+
+p.mode = mode
+
+head = head + 1
+
+local strs = {}
+
+local lines = 0
+local chars = 0
+while arg[head] ~= nil do
+    local file = arg[head]
+    head = head + 1
+    local data = read(file)
+    lines = lines + line_count(data)
+    chars = chars + string.len(data)
+    strs[#strs + 1] = {
+        file = file,
+        data = data
+    }
+end
+
+local t1 = os.clock()
+for nth=1, n do
+    for i=1, #strs do
+        local ok, res = pcall(function()
+            return p:parse(strs[i].data)
+        end)
+        if ok then
+            if echo then
+                print(tree.format(res))
+            end
+        else
+            print('fail ' .. strs[i].file)
+        end
+    end
+end
+local t2 = os.clock()
+if time then
+    print(string.format('files: %sms', math.floor((t2-t1)*1000)))
+    print(string.format('%s ln/s', math.floor(lines*n/(t2-t1))))
+    print(string.format('%s chars/ms', math.floor(chars*n/((t2-t1)*1000))))
 end
